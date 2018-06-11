474a9d2834d189464ba0e39e809dd6ec200498ea
Niek den Breeje <n.j.den.breeje@gmail.com>
2018-06-11 21:06:30 +0200

    L12: Validation errors and old data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,6 +43,15 @@
                             </a>
                         </li>
 
+                        <li class="nav-item">
+                            <a
+                                class="nav-link"
+                                href="{{ route('threads.create') }}"
+                            >
+                                New Thread
+                            </a>
+                        </li>
+
                         <li class="nav-item dropdown">
                             <a
                                 class="nav-link dropdown-toggle"
